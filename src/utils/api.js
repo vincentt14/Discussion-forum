a1ed95bba93f7d4ad07399315225629e0215163b
vincentt14 <vincent14@student.umn.ac.id>
2023-01-12 22:04:46 +0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,7 +19,55 @@ const api = (() => {
     return localStorage.getItem('accessToken');
   }
 
-  async function register({  })
+  async function register({ id, name, password }) {
+    const response = await fetch(`${BASE_URL}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+        name,
+        password,
+      }),
+    });
+
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { user } } = responseJson;
+
+    return user;
+  }
+
+  async function login({ id, password }) {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+        password,
+      }),
+    });
+
+    const responseJson = await response.json();
+
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { token } } = responseJson;
+
+    return token;
+  }
 
 })();
 
